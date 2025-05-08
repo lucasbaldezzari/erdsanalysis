09f313f46cd05fb1e60920acae930b8bf217d26b
lucasbaldezzari <lmbaldezzari@gmail.com>
2025-05-08 15:50:40 -0300

    Se agrega estudio en frecuencia

--- codes/erds.py
+++ codes/erds.py
@@ -22,7 +22,7 @@ sfreq = 512
 channels_to_drop = ["FP1","FP2","FPz","Fz","F8","F7","AF3","AF4","AF5","AF7","AF8","T7","T8","F9","F10"]
 pick = ["FC5","FC3","FC1","FCz","FC2","FC4","FC6","C5","C3","C1","Cz","C2","C4","C6","CP5","CP3","CP1","CPz","CP2","CP4","CP6",]
 
-eeg_concatenados = concatenateEEGs(sujeto, sesion, apply_ica=False).pick(pick,"ignore")
+eeg_concatenados = concatenateEEGs(sujeto, sesion, apply_ica=False).drop_channels(channels_to_drop, "ignore")#.pick(pick,"ignore")
 # eeg_concatenados.plot_sensors(kind="topomap",show_names=True) ##probar con kind="3d"
 # plotEEG(eeg_concatenados, show=True, scalings=40, bad_color = "red")
 
@@ -134,31 +134,31 @@ psd_der_task, _ = mne.time_frequency.psd_array_multitaper(trials_der_task.get_da
 psd_der_postask, _ = mne.time_frequency.psd_array_multitaper(trials_der_postask.get_data(), sfreq=sfreq, fmin=l_freq, fmax=h_freq, n_jobs=1)
 
 ##convertimos a db
-psd_izq_rest_db = 10 * np.log10(psd_izq_rest).mean(axis=0)
-psd_izq_task_db = 10 * np.log10(psd_izq_task).mean(axis=0)
-psd_izq_postask_db = 10 * np.log10(psd_izq_postask).mean(axis=0)
-psd_der_rest_db = 10 * np.log10(psd_der_rest).mean(axis=0)
-psd_der_task_db = 10 * np.log10(psd_der_task).mean(axis=0)
-psd_der_postask_db = 10 * np.log10(psd_der_postask).mean(axis=0)
+psd_izq_rest_pot = (psd_izq_rest).mean(axis=0)
+psd_izq_task_pot = (psd_izq_task).mean(axis=0)
+psd_izq_postask_pot = (psd_izq_postask).mean(axis=0)
+psd_der_rest_pot = (psd_der_rest).mean(axis=0)
+psd_der_task_pot = (psd_der_task).mean(axis=0)
+psd_der_postask_pot = (psd_der_postask).mean(axis=0)
 
 crest, ctask, cposttask ="#5dade2", "#45b27b", "#e74c3c"
 fig, axes = plt.subplots(2, 2, figsize=(14, 8))
-axes[0, 0].plot(freqline_rest, psd_izq_rest_db[c4_index, :], label="Baseline", color=crest, linestyle='--')
-axes[0, 0].plot(freqline_task, psd_izq_task_db[c4_index, :], label="Tarea", color=ctask, linewidth=2)
+axes[0, 0].plot(freqline_rest, psd_izq_rest_pot[c4_index, :], label="Baseline", color=crest, linestyle='--')
+axes[0, 0].plot(freqline_task, psd_izq_task_pot[c4_index, :], label="Tarea", color=ctask, linewidth=2)
 # Líneas de intervalo de confianza
 axes[0, 0].set_title("Baseline y Cue IZQUIERDA - C4")
-axes[0, 1].plot(freqline_rest, psd_izq_rest_db[c4_index, :], label="Baseline", color=crest, linestyle='--')
-axes[0, 1].plot(freqline_postask, psd_izq_postask_db[c4_index, :], label="Tarea", color=cposttask, linewidth=2)
+axes[0, 1].plot(freqline_rest, psd_izq_rest_pot[c4_index, :], label="Baseline", color=crest, linestyle='--')
+axes[0, 1].plot(freqline_postask, psd_izq_postask_pot[c4_index, :], label="Tarea", color=cposttask, linewidth=2)
 axes[0, 1].set_title("Baseline y Post Tarea IZQUIERDA - C4")
-axes[1, 0].plot(freqline_rest, psd_der_rest_db[c3_index, :], label="Baseline", color=crest, linestyle='--')
-axes[1, 0].plot(freqline_task, psd_der_task_db[c3_index, :], label="Tarea", color=ctask, linewidth=2)
+axes[1, 0].plot(freqline_rest, psd_der_rest_pot[c3_index, :], label="Baseline", color=crest, linestyle='--')
+axes[1, 0].plot(freqline_task, psd_der_task_pot[c3_index, :], label="Tarea", color=ctask, linewidth=2)
 axes[1, 0].set_title("Baseline y Cue DERECHA - C3")
-axes[1, 1].plot(freqline_rest, psd_der_rest_db[c3_index, :], label="Baseline", color=crest, linestyle='--')
-axes[1, 1].plot(freqline_postask, psd_der_postask_db[c3_index, :], label="Post Tarea", color=cposttask, linewidth=2)
+axes[1, 1].plot(freqline_rest, psd_der_rest_pot[c3_index, :], label="Baseline", color=crest, linestyle='--')
+axes[1, 1].plot(freqline_postask, psd_der_postask_pot[c3_index, :], label="Post Tarea", color=cposttask, linewidth=2)
 axes[1, 1].set_title("Baseline y Post Tarea DERECHA - C3")
 for ax in axes.flat:
     ax.set_xlabel("Frecuencia (Hz)")
-    ax.set_ylabel("Potencia (dB)")
+    ax.set_ylabel("Potencia $uV^2$")
     ax.axhline(0, color='grey', linestyle='--')
     ax.axvline(l_freq+1, color='grey', linestyle='--')
     ax.legend()
@@ -168,22 +168,22 @@ plt.show()
 
 ##reactive band selection
 # Umbral para significancia estadística
-diff_freqs_cuebase_izq = np.log10(psd_izq_task)[:,c4_index,:].mean(axis=0) - np.log10(psd_izq_rest)[:,c4_index,:].mean(axis=0)
+diff_freqs_cuebase_izq = (psd_izq_task)[:,c4_index,:].mean(axis=0) - (psd_izq_rest)[:,c4_index,:].mean(axis=0)
 mean_diff_cuebase_izq = np.mean(diff_freqs_cuebase_izq)
 std_cuebase_izq = np.std(diff_freqs_cuebase_izq) / np.sqrt(diff_freqs_cuebase_izq.shape[0])
 conf_interval_cuebase_izq = 1.96 * std_cuebase_izq
 ##repito para cuebase vs posttask izq
-diff_freqs_cuepostask_izq = np.log10(psd_izq_postask)[:,c4_index,:].mean(axis=0) - np.log10(psd_izq_rest)[:,c4_index,:].mean(axis=0)
+diff_freqs_cuepostask_izq = (psd_izq_postask)[:,c4_index,:].mean(axis=0) - (psd_izq_rest)[:,c4_index,:].mean(axis=0)
 mean_diff_cuepostask_izq = np.mean(diff_freqs_cuepostask_izq)
 std_cuepostask_izq = np.std(diff_freqs_cuepostask_izq) / np.sqrt(diff_freqs_cuepostask_izq.shape[0])
 conf_interval_cuepostask_izq = 1.96 * std_cuepostask_izq
 ##repito para derecha
-diff_freqs_cuebase_der = np.log10(psd_der_task)[:,c3_index,:].mean(axis=0) - np.log10(psd_der_rest)[:,c3_index,:].mean(axis=0)
+diff_freqs_cuebase_der = (psd_der_task)[:,c3_index,:].mean(axis=0) - (psd_der_rest)[:,c3_index,:].mean(axis=0)
 mean_diff_cuebase_der = np.mean(diff_freqs_cuebase_der)
 std_cuebase_der = np.std(diff_freqs_cuebase_der) / np.sqrt(diff_freqs_cuebase_der.shape[0])
 conf_interval_cuebase_der = 1.96 * std_cuebase_der
 ##repito para cuebase vs posttask der
-diff_freqs_cuepostask_der = np.log10(psd_der_postask)[:,c3_index,:].mean(axis=0) - np.log10(psd_der_task)[:,c3_index,:].mean(axis=0)
+diff_freqs_cuepostask_der = (psd_der_postask)[:,c3_index,:].mean(axis=0) - (psd_der_task)[:,c3_index,:].mean(axis=0)
 mean_diff_cuepostask_der = np.mean(diff_freqs_cuepostask_der)
 std_cuepostask_der = np.std(diff_freqs_cuepostask_der) / np.sqrt(diff_freqs_cuepostask_der.shape[0])
 conf_interval_cuepostask_der = 1.96 * std_cuepostask_der
@@ -212,7 +212,7 @@ axes[1, 1].axhline(mean_diff_cuepostask_der - conf_interval_cuepostask_der, colo
 axes[1, 1].set_title("Diferencia Baseline y Post Tarea DERECHA - Electrodo C3", fontsize=12)
 for ax in axes.flat:
     ax.set_xlabel("Frecuencia (Hz)")
-    ax.set_ylabel("Diferencia (dB)")
+    ax.set_ylabel("Potencia ($uV^2$)")
     ax.legend()
     ax.grid()
 plt.tight_layout()
@@ -242,9 +242,51 @@ for trial in range(tfr_der_data_1012.shape[0]):
 tfr_der_data_1012_c3 = tfr_der_data_1012.mean(axis=0)[c3_index, :]
 
 window_size = 512
+color_izq, color_der = "#5dade2", "#e74c3c"
+plt.figure(figsize=(10, 5))
 tfr_izq_data_1012_c4 = np.convolve(tfr_izq_data_1012_c4, np.ones(window_size)/window_size, mode='same')
 tfr_der_data_1012_c3 = np.convolve(tfr_der_data_1012_c3, np.ones(window_size)/window_size, mode='same')
-plt.plot(tfr_izq.times, tfr_izq_data_1012_c4, label="IZQUIERDA - C4", color="blue")
-plt.plot(tfr_der.times, tfr_der_data_1012_c3, label="DERECHA - C3", color="red")
+plt.plot(tfr_izq.times, tfr_izq_data_1012_c4, label="IZQUIERDA - C4", color=color_izq, linewidth=2)
+plt.plot(tfr_der.times, tfr_der_data_1012_c3, label="DERECHA - C3", color=color_der, linewidth=2)
+#linea vertical en 0, en -0.5 y en 2
+plt.axvline(0, color='k', linestyle='--', label='Cue onset')
+plt.axvline(-0.5, color='grey', linestyle='--')
+plt.axvline(2, color='grey', linestyle='--')
+plt.axhline(0, color='grey', linestyle='--')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('ERDS (%)')
+plt.title('Curvas ERDS% en C3 y C4')
+plt.grid()
 plt.legend()
 plt.show()
+
+### Aplicando baseline a "mano" a los datos de TFR para graficar los mapas topográficos
+tfr_izq_wbaseline = tfr_izq.copy()
+tfr_der_wbaseline = tfr_der.copy()
+
+tfr_izq_wbaseline.data.shape
+for trial in range(tfr_izq_wbaseline.data.shape[0]):
+    for freq in range(tfr_izq_wbaseline.data.shape[2]):
+        baseline_mean = baseline.apply(tfr_izq_wbaseline.data[trial, :, freq, :], tfr_izq_wbaseline.times)
+        tfr_izq_wbaseline.data[trial, :, freq, :] = 100*(tfr_izq_wbaseline.data[trial, :, freq, :] - baseline_mean) / baseline_mean
+
+for trial in range(tfr_der_wbaseline.data.shape[0]):
+    for freq in range(tfr_der_wbaseline.data.shape[2]):
+        baseline_mean = baseline.apply(tfr_der_wbaseline.data[trial, :, freq, :], tfr_der_wbaseline.times)
+        tfr_der_wbaseline.data[trial, :, freq, :] = 100*(tfr_der_wbaseline.data[trial, :, freq, :] - baseline_mean) / baseline_mean
+
+tfr_der_wbaseline.average().plot_topo(baseline=baseline_rest, mode='mean', title="TFR DERECHA - Baseline", colorbar=True, vmin=-50, vmax=50)
+
+tfr_der_wbaseline.average().plot_joint(
+    baseline=(-1.5, -0.5), mode="mean", tmin=-2, tmax=3, timefreqs=[(-0.5, 10), (-0.5, 12),(0.5, 10), (0.5, 12),(2, 10), (2, 12)]
+)
+
+tfr_der_wbaseline.average().plot_joint(picks="C3",
+    baseline=(-1.5, -0.5), mode="mean", tmin=-2, tmax=3, timefreqs=[(-1.5,12),(-0.5, 12),(0,12),(1,12),(1.5,12)])
+tfr_der_wbaseline.average().plot_joint(picks="C3",
+    baseline=(-1.5, -0.5), mode="mean", tmin=-2, tmax=3, timefreqs=[(-1.5,25),(-0.5, 25),(0,25),(1,25),(1.5,25)])
+
+tfr_izq_wbaseline.average().plot_joint(picks="C4",
+    baseline=(-1.5, -0.5), mode="mean", tmin=-2, tmax=3, timefreqs=[(-1.5,12),(-0.5, 12),(0,12),(1,12),(1.5,12)])
+tfr_izq_wbaseline.average().plot_joint(picks="C4",
+    baseline=(-1.5, -0.5), mode="mean", tmin=-2, tmax=3, timefreqs=[(-1.5,25),(-0.5, 25),(0,25),(1,25),(1.5,25)])
