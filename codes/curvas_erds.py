2f47abb8439afa8f0b1df601c3112cc445aa910b
lucasbaldezzari <lmbaldezzari@gmail.com>
2025-05-19 12:08:16 -0300

    Se cierra curvas_erds

--- codes/curvas_erds.py
+++ codes/curvas_erds.py
@@ -6,16 +6,20 @@ from mne.time_frequency import tfr_morlet
 from codes.utils import Baseline
 from matplotlib.colors import TwoSlopeNorm
 from codes.utils import LaplacianFilter, EnvolventeEEG
+import json
 
 ## 1. ******* CARGAMOS Y CONCATENAMOS LOS DATOS PARA EL/LA SUJETO EN CUESTIÓN *******
-sujeto=8
-sesion=2
-sfreq = 512
+##cargo codes\\parameters.json
+with open('codes\\parameters.json', 'r') as f:
+    parameters = json.load(f)
 
+sujeto = parameters["sujeto"]
+sesion = parameters["sesion"]
 tipo_sesion = "Ejecutada" if sesion == 1 else "Imaginada"
-
-channels_to_drop = ["FP1","FP2","FPz","Fz","F8","F7","AF3","AF4","AF5","AF7","AF8","T7","T8","F9","F10"]
-pick = ["FC5","FC3","FC1","FCz","FC2","FC4","FC6","C5","C3","C1","Cz","C2","C4","C6","CP5","CP3","CP1","CPz","CP2","CP4","CP6",]
+sfreq = 512
+channels_to_drop = parameters["channels_to_drop"]
+pick = parameters["pick"]
+confidence = parameters["confidence"]
 
 eeg_concatenados = concatenateEEGs(sujeto, sesion, apply_ica=False).drop_channels(channels_to_drop, "ignore")#.pick(pick,"ignore")
 # eeg_concatenados.plot_sensors(kind="topomap",show_names=True) ##probar con kind="3d"
@@ -29,7 +33,7 @@ eeg_concatenados = concatenateEEGs(sujeto, sesion, apply_ica=False).drop_channel
 
 # plotEEG(eeg_concatenados, show=True, scalings=40, bad_color = "red")
 
-l_freq, h_freq = 7, 32
+l_freq, h_freq = parameters["banda_completa"]
 eeg_concatenados.filter(l_freq=l_freq, h_freq=h_freq,
            picks="eeg", 
            method="fir", 
@@ -43,7 +47,7 @@ eeg_concatenados.filter(l_freq=l_freq, h_freq=h_freq,
 ## 2. ************************ SEPARANDO EN ÉPOCAS ************************
 
 ##epOching de eeg_concatenados
-tmin, tmax = -3, 4
+tmin, tmax = parameters["duracion_trial"]
 event_ids = dict(IZQUIERDA=1, DERECHA=2)
 epocas = mne.Epochs(eeg_concatenados, event_id=["IZQUIERDA", "DERECHA"],
                                  tmin=tmin-0.1, tmax=tmax+0.1,
@@ -52,104 +56,74 @@ epocas = mne.Epochs(eeg_concatenados, event_id=["IZQUIERDA", "DERECHA"],
 raw_eventos = mne.events_from_annotations(eeg_concatenados, event_id=event_ids)
 eventos=mne.pick_events(raw_eventos[0], include=[1,2])
 
-epocas.plot(scalings = 80,show=True, block=True,
-                          events=eventos,
-                          event_id=event_ids,
-                          event_color=dict(IZQUIERDA="red", DERECHA="blue"))
+# epocas.plot(scalings = 80,show=True, block=True,
+#                           events=eventos,
+#                           event_id=event_ids,
+#                           event_color=dict(IZQUIERDA="red", DERECHA="blue"))
 
-electrodos = ["CP3","CP4"]
-e1_index = eeg_concatenados.ch_names.index(electrodos[0])
-e2_index = eeg_concatenados.ch_names.index(electrodos[1])
+chs_names = parameters["electrodos"]
+chizq_n = chs_names[0] #nombre del canal izquierdo
+chder_n = chs_names[1] #nombre del canal izquierdo
+chizq_i = [eeg_concatenados.ch_names.index(chs_names[0])] #índice canal izquierdo
+chder_i = [eeg_concatenados.ch_names.index(chs_names[1])] #índice canal derecho
+cluster_izq = [eeg_concatenados.ch_names.index(ch) for ch in parameters["cluster_electrodos_izq"]]
+cluster_der = [eeg_concatenados.ch_names.index(ch) for ch in parameters["cluster_electrodos_der"]]
 clase_izquierda = epocas["IZQUIERDA"]
 clase_derecha = epocas["DERECHA"]
 clase_izquierda_avg = clase_izquierda.average()
 clase_derecha_avg= clase_derecha.average()
 
-clase_izquierda_avg.plot(picks=electrodos, show=True, spatial_colors=True)
-clase_izquierda_avg.plot_psd(fmax=100,picks=["C3","CP3","C4","CP4"], show=True, spatial_colors=True)
 
 ## 3. ************************ CURVAS ERDS% USANDO HILBERT ************************
 # Graficar curva ERDS para un canal específico (por ejemplo, C3)
-baseline = Baseline((-2, -1))  # Intervalo de tiempo para el baseline
-base = baseline.apply(epocas.get_data().mean(axis=0),clase_izquierda.times)
-erds_izq = getHilbertERDS(clase_izquierda, baseline, apply_smooth=True, window_smoothing=51, mean_trials=True)
-erds_der = getHilbertERDS(clase_derecha, baseline, apply_smooth=True, window_smoothing=51, mean_trials=True)
+  # Intervalo de tiempo para el baseline
+baseline_rest = parameters["baseline_rest"]  # Intervalo de tiempo para el baseline
+baseline_pretask = parameters["baseline_pretask"]  # Intervalo de tiempo para el baseline
+baseline_task = parameters["baseline_task"]
+baseline_postask = parameters["baseline_postask"]
+baseline = Baseline(tuple(baseline_rest))
+erds_izq = getHilbertERDS(clase_izquierda, baseline, apply_smooth=True, window_smoothing=102, mean_trials=True)
+erds_der = getHilbertERDS(clase_derecha, baseline, apply_smooth=True, window_smoothing=102, mean_trials=True)
 
-ti, tf = -0.5, 2.5
+
+c_ei, c_ed = parameters["colores_clases"] #colores para electrodos izquierdo y derecho
+cmap = parameters["cmap_topomaps"]
+sombra_cue = "#fdf88c"
 times = clase_izquierda.times
+ti, tf = -1.6, tmax
 idx = np.where((times >= ti) & (times <= tf))[0]
-plt.figure(figsize=(10, 5))
-plt.plot(times[idx], erds_izq[e1_index, idx], label="IZQUIERDA")
-plt.plot(times[idx], erds_der[e1_index, idx], label="DERECHA")
-plt.axvline(0, color="k", linestyle="--", label="Cue onset")
-plt.axhline(0, color="grey", linestyle="--")
-plt.xlabel("Tiempo (s)")
-plt.ylabel("ERDS (%)")
-plt.title(f"EDRS% {electrodos[0]} - Banda ({l_freq}-{h_freq}) Hz - Sesión {tipo_sesion}", fontsize=14)
-plt.legend(loc="upper right")
-# plt.xlim(-1, 3)
-plt.ylim(-70, 70)
-plt.grid()
-plt.show()
+i_times = np.where((times >= ti) & (times <= tf))[0]
+fig, axes = plt.subplots(1, 2, figsize=(5, 6))
+axes[0].plot(times[idx], erds_izq[cluster_izq].mean(0)[idx], label="IZQUIERDA", color=c_ei, linewidth=2)
+axes[0].plot(times[idx], erds_der[cluster_izq].mean(0)[idx], label="DERECHA", color=c_ed, linewidth=2)
+axes[0].axvline(0, color="k", linestyle="--", label="Cue onset")
+axes[0].axhline(0, color="grey", linestyle="--")
+axes[0].spines['top'].set_visible(False)
+axes[0].spines['right'].set_visible(False)
+axes[0].set_xlabel("Tiempo (s)")
+axes[0].set_ylabel(r"ERDS (%) $\mu V$")
+axes[0].set_title(f"EDRS% Cluster Izq - ({l_freq}-{h_freq})Hz - Sesión {tipo_sesion}", fontsize=14)
+ymin = min(erds_izq[cluster_izq].mean(0)[idx].min(), erds_der[cluster_izq].mean(0)[idx].min())
+ymax = max(erds_izq[cluster_izq].mean(0)[idx].max(), erds_der[cluster_izq].mean(0)[idx].max())
+axes[0].fill_between(times, ymin, ymax, where=(times >= -0.5) & (times <= 0), color='grey', alpha=0.2)
+axes[0].fill_between(times, ymin, ymax, where=(times >= 0) & (times<= 2), color=sombra_cue, alpha=0.2, label="Tarea")
+axes[0].legend(loc="lower right")
 
-plt.figure(figsize=(10, 5))
-plt.plot(times[idx], erds_izq[e2_index, idx], label="IZQUIERDA")
-plt.plot(times[idx], erds_der[e2_index, idx], label="DERECHA")
-plt.axvline(0, color="k", linestyle="--", label="Cue onset")
-plt.axhline(0, color="grey", linestyle="--")
-plt.xlabel("Tiempo (s)")
-plt.ylabel("ERDS (%)")
-plt.title(f"EDRS% {electrodos[1]} - Banda ({l_freq}-{h_freq}) Hz - Sesión {tipo_sesion}")
-plt.legend(loc="upper right")
-# plt.xlim(-1, 3)
-plt.ylim(-70, 70)
-plt.grid()
-plt.show()
+axes[1].plot(times[idx], erds_izq[cluster_der].mean(0)[idx], label="IZQUIERDA", color=c_ei, linewidth=2)
+axes[1].plot(times[idx], erds_der[cluster_der].mean(0)[idx], label="DERECHA", color=c_ed, linewidth=2)
+axes[1].axvline(0, color="k", linestyle="--", label="Cue onset")
+axes[1].axhline(0, color="grey", linestyle="--")
+axes[1].spines['top'].set_visible(False)
+axes[1].spines['left'].set_visible(False)
+axes[1].yaxis.tick_right()
+axes[1].yaxis.set_label_position("right")
+axes[1].set_xlabel("Tiempo (s)")
+axes[1].set_ylabel(r"ERDS (%) $\mu V$")
+axes[1].set_title(f"EDRS% Cluster DER - ({l_freq}-{h_freq})Hz - Sesión {tipo_sesion}", fontsize=14)
+ymin = min(erds_izq[cluster_der].mean(0)[idx].min(), erds_der[cluster_der].mean(0)[idx].min())
+ymax = max(erds_izq[cluster_der].mean(0)[idx].max(), erds_der[cluster_der].mean(0)[idx].max())
+axes[1].fill_between(times, ymin, ymax, where=(times >= -0.5) & (times <= 0), color='grey', alpha=0.2)
+axes[1].fill_between(times, ymin, ymax, where=(times >= 0) & (times<= 2), color=sombra_cue, alpha=0.2, label="Tarea")
+axes[1].legend(loc="lower right")
 
-chans = ["C2","C4","CP4"]#["C1","C3","CP3"]
-data_izquierda=clase_izquierda_avg.get_data(picks=chans)
-data_derecha=clase_derecha_avg.get_data(picks=chans)
-# window = 512
-# #aplicamos convolución a los datos
-# for i, chan in enumerate(chans):
-#     data_derecha[i] = np.convolve(data_derecha[i], np.ones(window)/window, mode='same')
-#     data_izquierda[i] = np.convolve(data_izquierda[i], np.ones(window)/window, mode='same')
-
-ti, tf = -1.5, 2.5
-times = clase_izquierda.times
-idx = np.where((times >= ti) & (times <= tf))[0]
-##grafico de filas=nchans y 1 columna
-fig, axes = plt.subplots(nrows=len(chans), ncols=1, figsize=(10, 5), sharex=True)
-for i, chan in enumerate(chans):
-    e_index = clase_derecha_avg.ch_names.index(chan)
-    axes[i].plot(clase_derecha_avg.times[idx], data_izquierda[i, idx], label="IZQUIERDA")
-    axes[i].plot(clase_derecha_avg.times[idx], data_derecha[i, idx], label="DERECHA")
-    axes[i].axvline(0, color="k", linestyle="--", label="Cue onset")
-    axes[i].axhline(0, color="grey", linestyle="--")
-    axes[i].set_ylabel("ERDS (%)")
-    axes[i].set_title(f"EDRS% {chan} - Banda ({l_freq}-{h_freq}) Hz - Sesión {tipo_sesion}", fontsize=14)
-    axes[i].legend(loc="upper right")
-    # plt.xlim(-1, 3)
-    # axes[i].set_ylim(-70, 70)
-    axes[i].grid()  
-plt.xlabel("Tiempo (s)")
-plt.tight_layout()
-plt.show()
-
-chans = ["C1","C3","CP3"]
-fig, axes = plt.subplots(nrows=len(chans), ncols=1, figsize=(10, 5), sharex=True)
-for i, chan in enumerate(chans):
-    e_index = clase_derecha_avg.ch_names.index(chan)
-    axes[i].plot(clase_derecha_avg.times[idx], data_izquierda[i, idx], label="IZQUIERDA")
-    axes[i].plot(clase_derecha_avg.times[idx], data_derecha[i, idx], label="DERECHA")
-    axes[i].axvline(0, color="k", linestyle="--", label="Cue onset")
-    axes[i].axhline(0, color="grey", linestyle="--")
-    axes[i].set_ylabel("ERDS (%)")
-    axes[i].set_title(f"EDRS% {chan} - Banda ({l_freq}-{h_freq}) Hz - Sesión {tipo_sesion}", fontsize=14)
-    axes[i].legend(loc="upper right")
-    # plt.xlim(-1, 3)
-    # axes[i].set_ylim(-70, 70)
-    axes[i].grid()  
-plt.xlabel("Tiempo (s)")
-plt.tight_layout()
 plt.show()
